208c4334bd9c9e284781dbeaafc485e67cbaef02
JoeyKraemer <joey.kramer@student.nhlstenden.com>
2022-12-09 15:28:28 +0100

    minor improvements to databasestructur, delete columns, add gitignore line

--- DatabaseStructure.sql
+++ DatabaseStructure.sql
@@ -1,6 +1,6 @@
 -- Please import this statement to your database via PHPmyAdmin
 
-DROP DATABSE IF EXISTS `webapplication`
+DROP DATABASE IF EXISTS `webapplication`;
 -- Create database change value in ` ` to your needs 
 CREATE DATABASE IF NOT EXISTS `webapplication` DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci;
 SET FOREIGN_KEY_CHECKS=0;
@@ -30,7 +30,9 @@ CREATE TABLE `Event` (
    location_street VARCHAR(50),
    location_postal_code VARCHAR(6),
    location_city VARCHAR(30),
-   claimed BOOLEAN,
+   event_time TIME,
+   event_date DATE,
+   inactive BOOLEAN,
    PRIMARY KEY(event_id)
 );
 
@@ -38,7 +40,6 @@ CREATE TABLE `Event` (
 
 CREATE TABLE `User` (
    user_id INT AUTO_INCREMENT NOT NULL,
-   user_name VARCHAR(25) UNIQUE NOT NULL,
    user_password VARCHAR(60) NOT NULL,
    password_change_date TIMESTAMP,
    first_name VARCHAR(25) NOT NULL,
@@ -57,7 +58,7 @@ CREATE TABLE `Event_Details` (
    event_details_id INT AUTO_INCREMENT NOT NULL,
    event_id INT NOT NULL,
    user_id INT NOT NULL,
-   Checkin_date TIMESTAMP NOT NULL,
+   checkin_date TIMESTAMP NOT NULL,
    PRIMARY KEY(event_details_id),
    FOREIGN KEY(event_id) REFERENCES Event(event_id) ON UPDATE CASCADE ON DELETE NO ACTION,
    FOREIGN KEY(user_id) REFERENCES User(user_id) ON UPDATE CASCADE ON DELETE NO ACTION
