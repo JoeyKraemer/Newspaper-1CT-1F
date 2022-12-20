183a67313743b52650f21a7913307db64881dda3
beyzaolmez <beyza.olmez@student.nhlstenden.com>
2022-12-20 18:59:38 +0100

    finished the frontend for the password reset page

---
+++ reset.php
@@ -0,0 +1,30 @@
+
+<!doctype html>
+<html>
+<head>
+    <meta charset="UTF8">
+    <title> Gemorskos header </title>
+    <link rel= "stylesheet" href="css/reset.css">
+</head>
+<body>
+    <div id="container">
+        <main>
+        <div id="content">
+        
+            <h1> Gemorskos Intranet </h1>
+            
+            <p> Welcome back. Please enter your new password and go back to login. </p>
+
+            <form action="#" method="POST">  
+            <input type="password name="pass" id="pass1" placeholder="Password">
+            <input type="password" name="passRepeat" id="pass2" placeholder="Confirm Password">
+            <input type="submit" id="submit" value="Submit"> 
+            </form>
+            
+            <button type="button"> <a href="login.php"> Back to login </a> </button>
+            
+        </div>		
+        </main>
+    </div>
+</body>
+</html>
