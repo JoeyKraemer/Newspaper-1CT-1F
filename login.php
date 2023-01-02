2c83889483f983c79fdbf6e5b7ceb93c98eeca79
beyzaolmez <beyza.olmez@student.nhlstenden.com>
2023-01-02 22:17:19 +0100

    removed the comments so the code matches the code guidelines

--- login.php
+++ login.php
@@ -6,10 +6,8 @@
     <link rel= "stylesheet" href="css/login.css">
 </head>
 <body>
-    <!-- grid -->
     <div id="container">
 
-        <!-- main section -->
         <main>
         <div id="content">
         
@@ -17,20 +15,17 @@
             
             <p> Please enter the required information and follow the instructions sent to your inbox for resetting your password </p>
 
-            <!-- form -->
             <form action="#" method="POST">  
             <input type="email” name="email" id="email" placeholder="Email">
             <input type="password" name="pass" id="pass" placeholder="Password">
 
             <input type="submit" id="submit" value="Log In"> 
             </form>
-            
-            <!-- pass reset link -->
+
             <a href="recover.php"> Forgot your password? </a>
             
         </div>		
         </main>
-
     </div>
 </body>
 </html>
