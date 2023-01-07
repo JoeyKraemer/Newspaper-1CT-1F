8f58190394d7ece60de3555c76d2cab0a38b22a7
NidLip <nikolay.minins@student.nhlstenden.com>
2023-01-07 19:42:57 +0200

    Implementing js to the code

---
+++ Calendar.php
@@ -0,0 +1,82 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link type="text/css" rel="stylesheet" href="CalendarCSS.css">
+    <link type="text/css" rel="stylesheet" href="normalize.css">
+    <title>Title</title>
+    <script src="https://code.jquery.com/jquery-3.6.3.js" integrity="sha256-nQLuAZGRRcILA+6dMBOvcRh5Pe310sBpanc6+QBmyVM=" crossorigin="anonymous"></script>
+</head>
+<body>
+<div id="container">
+
+    <header>
+    </header>
+    <main>
+        <div class="WelcomeUser">
+            <h2>Welcome, Firstname</h2>
+        </div>
+
+<div id="calendar">
+    <div id="dateAndEvents">
+        <div id="date"><span style="font-size: 3em;" id="calendarDate">22</span><h3>November <br> 2022</h3></div>
+        <div class="eventList">
+            <u>Upcoming Events</u>
+            <ul>Local Festival</ul>
+                <li>10:00-18:00</li>
+                <li>Event info</<li>
+        <br>
+        <u>My Schedule</u>
+        <ul>Local Festival</ul>
+            <li>10:00-18:00</li>
+            <li>Event info</li>
+        </div>
+    </div>
+    <div id="daysAndWeeks">
+        <table>
+            <tr>
+                <th>S</th>
+                <th>M</th>
+                <th>T</th>
+                <th>W</th>
+                <th>T</th>
+                <th>F</th>
+                <th>S</th>
+            </tr>
+            <?
+
+            echo "<tr>";
+            for ($i = 1; $i<=30; $i++) {
+                echo "<td data-day='$i'>$i</td>";
+                if ($i % 7 == 0) {
+                    echo "</tr><tr>";
+                }
+                if ($i == 30) {
+                    for ($j=0;$j<5;$j++) {
+                        echo "<td></td>";
+                    }
+                    echo "</tr>";
+                }
+            }
+
+            ?>
+        </table>
+    </div>
+</div>
+</main>
+    <footer>
+
+    </footer>
+</div>
+<script>
+    $('td').on('click', function(){
+        let selecteDate = $(this).data('day');
+        let dateTarget = $('#calendarDate');
+
+        dateTarget.text(selecteDate)
+    });
+</script>
+</body>
+</html>
+
---
+++ js/index.js
@@ -0,0 +1,13 @@
+/*
+document.addEventListener('DOMContentLoaded', function() {
+    var calendarEl = document.getElementById('calendar2');
+    var calendar = new FullCalendar.Calendar(calendarEl, {
+        initialView: 'dayGridMonth'
+    });
+    calendar.render();
+});
+ */
+
+$('.eventList').on('click', function(){
+   console.log('click event ');
+});
