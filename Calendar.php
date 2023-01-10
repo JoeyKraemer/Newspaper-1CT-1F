391f4d2b177a7dc606e6320d53ef2a487e98520b
guntzel-sebastian <sebastian.guntzel@student.nhlstenden.com>
2023-01-10 15:10:53 +0100

    Added basic php and mysql interaction

--- Calendar.php
+++ Calendar.php
@@ -1,3 +1,11 @@
+<?php
+    try{
+        $dbHandler = new PDO("mysql:host=mysql;dbname=gemorskos;charset=utf8", "root", "qwerty");
+    }
+    catch(Exception $ex){
+        echo "The following exception has occurred $ex";
+    }
+?>
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -9,6 +17,17 @@
     <script src="https://code.jquery.com/jquery-3.6.3.js" integrity="sha256-nQLuAZGRRcILA+6dMBOvcRh5Pe310sBpanc6+QBmyVM=" crossorigin="anonymous"></script>
 </head>
 <body>
+<?php
+    if(isset($dbHandler)){
+        try {
+            $stmt = $dbHandler->prepare("SELECT * FROM `Events`");
+            $stmt->execute();
+        }
+        catch(Exception $ex){
+            echo "The following exception has occurred $ex";
+        }
+    }
+?>
 <div id="container">
 
     <header>
@@ -22,11 +41,40 @@
     <div id="dateAndEvents">
         <div id="date"><span style="font-size: 3em;" id="calendarDate">22</span><h3>November <br> 2022</h3></div>
         <div class="eventList">
-            <u>Upcoming Events</u>
-            <ul>Local Festival</ul>
-                <li>10:00-18:00</li>
-                <li>Event info</<li>
+            <?php
+            if(isset($stmt)){
+                if($stmt->rowCount() > 0){
+                $results = $stmt->fetchAll(PDO::FETCH_ASSOC);
+                    foreach ($results as $result){
+                        echo "<u>Upcoming Events</u>";
+                        echo "<ul>" . $result["event_name"] . "</ul>";
+                        echo "<li>" . $result["event_description"] . "</li>";
+                        echo "<li>" . $result["location_street"] . "</li>";
+                        echo "<li>" . $result["location_postal_code"] . "</li>";
+                        echo "<li>" . $result["location_city"] . "</li>";
+                        echo "<li>" . $result["event_time"] . "</li>";
+                        echo "<li>" . $result["event_date"] . "</li>";
+                        echo "<li>" . $result["event_max_participant"] . "</li>";
+                        echo "<li>" . $result["active"] . "<  /li>";
+                    }
+                }
+                else{
+                    echo "No events scheduled";
+                }
+            }
+            ?>
         <br>
+            <?php
+            if(isset($dbHandler)){
+                try {
+                    $stmt = $dbHandler->prepare("SELECT * FROM `Events`");
+                    $stmt->execute();
+                }
+                catch(Exception $ex){
+                    echo "The following exception has occurred $ex";
+                }
+            }
+            ?>
         <u>My Schedule</u>
         <ul>Local Festival</ul>
             <li>10:00-18:00</li>
