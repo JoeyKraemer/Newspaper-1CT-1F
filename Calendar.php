a8832b6fce84608a28b9c628bfcbdb41c4a51d3b
guntzel-sebastian <sebastian.guntzel@student.nhlstenden.com>
2023-01-11 15:43:22 +0100

    Changed the way items are fetch from the database

--- Calendar.php
+++ Calendar.php
@@ -17,17 +17,6 @@
     <script src="https://code.jquery.com/jquery-3.6.3.js" integrity="sha256-nQLuAZGRRcILA+6dMBOvcRh5Pe310sBpanc6+QBmyVM=" crossorigin="anonymous"></script>
 </head>
 <body>
-<?php
-    if(isset($dbHandler)){
-        try {
-            $stmt = $dbHandler->prepare("SELECT * FROM `Events`");
-            $stmt->execute();
-        }
-        catch(Exception $ex){
-            echo "The following exception has occurred $ex";
-        }
-    }
-?>
 <div id="container">
 
     <header>
@@ -42,39 +31,27 @@
         <div id="date"><span style="font-size: 3em;" id="calendarDate">22</span><h3>November <br> 2022</h3></div>
         <div class="eventList">
             <?php
-            if(isset($stmt)){
-                if($stmt->rowCount() > 0){
-                $results = $stmt->fetchAll(PDO::FETCH_ASSOC);
-                    foreach ($results as $result){
-                        echo "<u>Upcoming Events</u>";
-                        echo "<ul>" . $result["event_name"] . "</ul>";
-                        echo "<li>" . $result["event_description"] . "</li>";
-                        echo "<li>" . $result["location_street"] . "</li>";
-                        echo "<li>" . $result["location_postal_code"] . "</li>";
-                        echo "<li>" . $result["location_city"] . "</li>";
-                        echo "<li>" . $result["event_time"] . "</li>";
-                        echo "<li>" . $result["event_date"] . "</li>";
-                        echo "<li>" . $result["event_max_participant"] . "</li>";
-                        echo "<li>" . $result["active"] . "<  /li>";
-                    }
-                }
-                else{
-                    echo "No events scheduled";
-                }
-            }
-            ?>
-        <br>
-            <?php
             if(isset($dbHandler)){
                 try {
-                    $stmt = $dbHandler->prepare("SELECT * FROM `Events`");
+                    $stmt = $dbHandler->prepare("SELECT * FROM `Events` WHERE event_date = :event_date");
+                    $stmt->bindParam("event_date", $_GET['event_date'], PDO::PARAM_STR);
+
                     $stmt->execute();
+                    $value = $stmt->fetch();
+                    $stmt->rowCount() > 0;
+
                 }
                 catch(Exception $ex){
                     echo "The following exception has occurred $ex";
                 }
             }
+            else{
+                echo "No events planned";
+            }
+
+            var_dump($value);
             ?>
+        <br>
         <u>My Schedule</u>
         <ul>Local Festival</ul>
             <li>10:00-18:00</li>
