9891cca48b2e8c42d7aaddb1412a9f08b233fcc9
Stefan Ionut Tasca <stefan.tasca@student.nhlstenden.com>
2023-01-13 12:29:45 +0100

    Grabbing user Role and preparing jobs

--- profilePage.php
+++ profilePage.php
@@ -1,5 +1,7 @@
 <?php
 
+$user_ID = 1;
+
 require_once("connection.php");
 
 global $dbHandler;
@@ -32,10 +34,12 @@ foreach($stmt-> fetchall() as $photo){
 					
 						<?php
 
-							$stmt = $dbHandler->prepare("SELECT first_name, last_name FROM `Users`");
+							$stmt = $dbHandler->prepare("SELECT first_name, last_name FROM `Users`
+																						WHERE Users.user_id = :userid");
+
+							$stmt->bindParam('userid', $user_ID, PDO::PARAM_STR);
 							$stmt -> execute();
 							$table = $stmt->setFetchMode(PDO::FETCH_OBJ); 
-
 
 								foreach($stmt->fetchall() as $tables){
 
@@ -43,21 +47,60 @@ foreach($stmt-> fetchall() as $photo){
 
 								}
 						?>
-						<div id="accountDetails">
-						<h2> Your Role: </h2>
-						<h2> Jobs: </h2>
-						<div class="jobBox">
-							<p>1 december-Wedding photographer in London</p> 
-						</div>
-						<div class="jobBox">
-							<p>1 september- Doing something</p>
-						</div>
+
+					<div id="accountDetails">
+
+						<?php
+
+							$stmt = $dbHandler->prepare("SELECT user_role, type_of_staff  FROM `Users`
+																						WHERE Users.user_id = :userid");
+
+							$stmt->bindParam('userid', $user_ID, PDO::PARAM_STR);
+							$stmt -> execute();
+							$job = $stmt->setFetchMode(PDO::FETCH_OBJ); 
+
+
+								foreach($stmt->fetchall() as $jobs){
+
+									echo '<h2> Your Role: ' . $jobs->user_role .' '.  '</h2>';
+									echo '<h2>  Jobs:</h2>';
+								}
+
+
+							$stmt = $dbHandler->prepare("SELECT Events.event_name, Events.event_description, Events.event_date 
+																						FROM Events, Event_Details, Users
+																						WHERE Events.event_id = Event_Details.event_id
+																						AND Event_Details.user_id = Users.user_id
+																						AND Users.user_id = :userid;");
+
+							$stmt->bindParam('userid', $user_ID, PDO::PARAM_STR);
+							$stmt -> execute();
+							$jobName = $stmt->setFetchMode(PDO::FETCH_OBJ); 
+
+
+								foreach($stmt->fetchall() as $jobName){
+
+									echo '<div class ="jobBox">';
+									echo '<p>' . $jobName->event_name .'    '.  '</p>'; 
+									echo '<p>' . $jobName->event_description  .'    '.  '</p>';
+									echo '<p>' . $jobName->event_date  .'    '.  '</p>';
+									echo '</div>';
+
+								}
+							?>
+						
 						 <a href="calendar.php"> Calendar/schedule </a>
 						 <a href="recover.php"> Forgot your password? </a>
+
+						 <form action="file.php" enctype= "multipart/form-data" method = "POST">
+
+							<input type="file" name = "file">
+							<input type="submit" name= "submit">
+
+						 </form>
 					</div>
 				</div>
 			</main>
 		</div>
   </body>
 </html>
-
